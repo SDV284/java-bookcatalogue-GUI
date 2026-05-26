928b6afee196c6a71e7f65dbd0c87d044bd544fa
gilbert <sdv71433@gmail.com>
2026-05-26 21:22:00 +0300

    Add Publication class

---
+++ src/main/java/com/bookcatalogueGUI/Publication.java
@@ -0,0 +1,36 @@
+package com.bookcatalogueGUI;
+
+import java.io.Serializable;
+
+public class Publication implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private String title;
+    private int year;
+
+    public Publication(String title, int year) {
+        this.title = title;
+        this.year = year;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Публікація: \"%s\" (%d р.)", title, year);
+    }
+}
