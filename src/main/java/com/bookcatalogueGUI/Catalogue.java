da3782563275e78f37c829f848b45839a4d5e2f7
gilbert <sdv71433@gmail.com>
2026-05-26 21:27:50 +0300

    Add serialization and deserialization methods to Catalogue

--- src/main/java/com/bookcatalogueGUI/Catalogue.java
+++ src/main/java/com/bookcatalogueGUI/Catalogue.java
@@ -1,5 +1,6 @@
 package com.bookcatalogueGUI;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -33,5 +34,18 @@ public class Catalogue {
             throw new BookNotFoundException("Публікацію з назвою \"" + title + "\" не знайдено.");
         }
         publications.remove(toRemove);
+    }
+
+    @SuppressWarnings("unchecked")
+    public void loadFromFile(String filename) throws IOException, ClassNotFoundException {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(filename))) {
+            publications = (List<Publication>) ois.readObject();
+        }
+    }
+
+    public void saveToFile(String filename) throws IOException {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(filename))) {
+            oos.writeObject(publications);
+        }
     }
 }
