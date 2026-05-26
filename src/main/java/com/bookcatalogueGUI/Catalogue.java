7d530d3e2070f928014679dde1ba7031067da5f3
gilbert <sdv71433@gmail.com>
2026-05-26 21:26:15 +0300

    Create Catalogue class skeleton with List

---
+++ src/main/java/com/bookcatalogueGUI/Catalogue.java
@@ -0,0 +1,12 @@
+package com.bookcatalogueGUI;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Catalogue {
+    private List<Publication> publications;
+
+    public Catalogue() {
+        this.publications = new ArrayList<>();
+    }
+}
